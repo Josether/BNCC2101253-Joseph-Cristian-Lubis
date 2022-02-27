acd2b4f5d6a71821fe46eb4261f15e17b6dab666
Joseph Lubis <josthigeth23@gmail.com>
2022-02-27 22:16:37 +0700

    mid project lnt backend

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts/app')
+
+@section('title', 'Halaman Home')
+
+@section('content')
+    <div class="container banner">
+        <h1>Welcome to Joseph's Library</h1>
+        <p>Kumpulan buku-buku terlengkap dari penulis lokal hingga internasional</p>
+
+        <a href="#" class="btn btn-info">Manage Books</a>
+        <a href="#" class="btn btn-warning">Contact Us</a>
+
+    </div>
+@endsection
